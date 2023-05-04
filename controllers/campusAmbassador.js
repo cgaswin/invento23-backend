d52069d6bb6f854f598ffde143474614bef909d1
cgaswin <cgaswin333@gmail.com>
2023-05-04 16:21:34 +0530

    fix db issues

--- controllers/campusAmbassador.js
+++ controllers/campusAmbassador.js
@@ -2,8 +2,8 @@ const campusAmbassadors = require("../models/campusAmbassador");
 const BigPromise = require("../middlewares/bigPromise");
 
 exports.getCampusAmbassadors = BigPromise(async (req, res, next) => {
-  const ambassadors = await campusAmbassadors.find({});
-  console.log(campusAmbassadors);
+  const ambassadors = await campusAmbassadors.find();
+  console.log(ambassadors)
     res.status(200).json({
       success: true,
       ambassadors,
