8773f06d2fa591a809d010864071b4d693450946
cgaswin <cgaswin333@gmail.com>
2023-05-05 20:37:51 +0530

    working on ejs file for creating events PS: not completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 require('dotenv').config()
 const morgan = require("morgan")
 const cookieParser = require("cookie-parser")
+const fileUpload = require("express-fileupload");
 const app = express()
 
 
@@ -9,6 +10,20 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(morgan("tiny"))
+
+app.use(
+    fileUpload({
+      useTempFiles: true,
+      tempFileDir: "/tmp/",
+    })
+  );
+
+  app.set("view engine", "ejs");
+
+  app.get("/api/v1/newevent", (req, res) => {
+    res.render("eventcreate");
+  });
+  
 
 //routes
 const campusAmbassador = require("./routes/campusAmbassador")
