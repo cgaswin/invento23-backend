2b887afa155238139a450cdf2011233464bed340
cgaswin <cgaswin333@gmail.com>
2023-05-06 18:42:27 +0530

    implemented functionality to add events

--- controllers/event.js
+++ controllers/event.js
@@ -1,56 +1,69 @@
-const Events = require("../models/event")
+const Events = require("../models/event");
 const BigPromise = require("../middlewares/bigPromise");
 const cloudinary = require("cloudinary").v2;
 const CustomError = require("../errors/customError");
 
-exports.getEvents = BigPromise(async (req,res,next) => {
-    const {category} = req.query
-    const queryObject = {}
-    if(category){
-        queryObject.category = category
-    }
+exports.getEvents = BigPromise(async (req, res, next) => {
+  const { category } = req.query;
+  const queryObject = {};
+  if (category) {
+    queryObject.category = category;
+  }
 
-    let events = await Events.find(queryObject)
-    return res.status(200).json({
-        success:true,
-        events
-    })
-})
+  let events = await Events.find(queryObject);
+  return res.status(200).json({
+    success: true,
+    events,
+  });
+});
 
-exports.addEvent = BigPromise(async(req,res,next) => {
+exports.addEvent = BigPromise(async (req, res, next) => {
 
-    if(!req.files){
-        return next(new CustomError("image is required",400))
-    }
+  if (!req.files) {
+    return next(new CustomError("image is required", 400));
+  }
 
-    const {name,date,regFee,eventType,category,isPreEvent,description,prize,rules} = req.body
+  const {
+    name,
+    date,
+    regFee,
+    eventType,
+    category,
+    isPreEvent,
+    description,
+    prize,
+    rules,
+  } = req.body;
 
+  
 
-    if(!name|| !date || !regFee || !eventType || !category || !isPreEvent || !description || !prize || !rules){
-        return next(new CustomError("all fields are mandatory",400))
-    }  
+  let file = req.files.photo;
 
-    let file = req.files.photo
+  if(!name|| !date || !regFee || !eventType || !category || !isPreEvent || !description || !prize || !rules){
+      return next(new CustomError("all fields are mandatory",400))
+  }
 
-    const result = await cloudinary.uploader.upload(file.tempFilePath,{
-        folder:"invento23"
-    })
-
-    const event = await Events.create({
-        name,
-        date,
-        regFee,
-        eventType,
-        category,
-        isPreEvent,
-        description,
-        prize,
-        rules,
-        photo: {
-            id: result.public_id,
-            secure_url: result.secure_url,
-          },
-    })
-})
+  let rulesArray = rules.split(",")
 
 
+  const result = await cloudinary.uploader.upload(file.tempFilePath, {
+    folder: "invento23",
+  });
+
+  const event = await Events.create({
+    name,
+    date,
+    regFee,
+    eventType,
+    category,
+    isPreEvent,
+    description,
+    photo: {
+        id: result.public_id,
+        secure_url: result.secure_url,
+      },
+    prize,
+    rules:rulesArray
+  });
+
+});
