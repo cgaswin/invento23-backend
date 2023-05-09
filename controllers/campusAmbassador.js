7bad30ecab72e79f6ac1a186812a9dd0401b7852
cgaswin <cgaswin333@gmail.com>
2023-05-09 15:01:27 +0530

    added controllers to create new campus ambassadors

--- controllers/campusAmbassador.js
+++ controllers/campusAmbassador.js
@@ -1,6 +1,40 @@
 const campusAmbassadors = require("../models/campusAmbassador");
 const BigPromise = require("../middlewares/bigPromise");
 
+
+exports.createCampusAmbassador = BigPromise(async (req,res,next)=>{
+  const {
+    name,
+    collegeName,
+    state,
+    district,
+    collegeType,
+    yearOfStudy,
+    contact,
+    email,
+    previousExperience,
+    referralCode,
+  } = req.body
+
+  const ambassador = await campusAmbassadors.create({
+    name,
+    collegeName,
+    state,
+    district,
+    collegeType,
+    yearOfStudy,
+    contact,
+    email,
+    previousExperience,
+    referralCode,
+  })
+
+  res.status(200).json({
+    success:true,
+    ambassador
+  })
+})
+
 exports.getCampusAmbassadors = BigPromise(async (req, res, next) => {
   const ambassadors = await campusAmbassadors.find();
   console.log(ambassadors)
