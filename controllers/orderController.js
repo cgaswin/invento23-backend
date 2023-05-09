dc9fbccafc2e74e548d6ac78b8c75e9b7785c5f1
cgaswin <cgaswin333@gmail.com>
2023-05-09 08:17:23 +0530

    added functionality to increment score of campus ambassadors

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,6 @@
 const Order = require("../models/order")
 const Events = require("../models/event")
+const campusAmbassador = require("../models/campusAmbassador")
 const BigPromise = require("../middlewares/bigPromise")
 
 exports.createOrder = BigPromise(async (req,res,next)=>{
@@ -40,14 +41,30 @@ exports.updateOrder = BigPromise(async (req,res,next)=>{
     order.orderEvents.forEach(async(event)=>{
         await updateEventTicket(event.id)
     })
+
+    if(order.refferalCode){
+       await updateCampusAmbassador(order.refferalCode)
+    }
     
+    await order.save()
+
+    res.status(200).json({
+        success:true,
+        order
+    })
 
 })
 
 async function updateEventTicket(eventId){
     const event = await Events.findById(eventId)
-
     event.ticketsBooked = event.ticketsBooked+1
 
     await event.save({validateBeforeSave:false})
+}
+
+async function updateCampusAmbassador(refferalCode){
+    const ambassador = await campusAmbassador.findById(refferalCode)
+    ambassador.score = ambassador.score + 10
+
+    await ambassador.save({validateBeforeSave:false})
 }
--- routes/order.js
+++ routes/order.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const {createOrder} = require("../controllers/orderController")
 
 router.route("/order/create").post(createOrder)
+router.route("/order/:id").put(updateOrder)
 
 
 
