f75be1ac5931befa24629ce360d4242dd420116a
cgaswin <cgaswin333@gmail.com>
2023-05-09 15:11:01 +0530

    added route for campus ambassador

--- routes/campusAmbassador.js
+++ routes/campusAmbassador.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
 
-const {getCampusAmbassadors} = require("../controllers/campusAmbassador")
+const {getCampusAmbassadors,createCampusAmbassador} = require("../controllers/campusAmbassador")
 
 router.route("/ambassadors").get(getCampusAmbassadors)
+router.route("/ambassadors/create").post(createCampusAmbassador)
+
 
 
 
