2c524a0441ebfcf58c3d9439d9eed36ff9fbc06b
cgaswin <cgaswin333@gmail.com>
2023-05-10 22:17:46 +0530

    minor change

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const {createUser,getUsers} = require("../controllers/user")
+const {getUsers} = require("../controllers/user")
 
 router.route("/users").get(getUsers)
 
