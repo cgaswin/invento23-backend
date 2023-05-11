e9eff9bce334d20b01837f50f9f665b7399c2ec7
cgaswin <cgaswin333@gmail.com>
2023-05-11 10:42:27 +0530

    small fix

--- routes/order.js
+++ routes/order.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { createOrder, getAllOrders } = require("../controllers/orderController");
 
-router.route("/orders").post(getAllOrders);
+router.route("/orders").get(getAllOrders);
 router.route("/order/create").post(createOrder);
 
 module.exports = router;
