e6c0363f294a9f9cb5c627e69dcde50ef294077b
cgaswin <cgaswin333@gmail.com>
2023-05-13 13:44:04 +0530

    mail fix probably for the final time

--- utils/emailHelper.js
+++ utils/emailHelper.js
@@ -1,57 +1,55 @@
-const sgMail = require("@sendgrid/mail")
-const fs = require("fs").promises
+const sgMail = require("@sendgrid/mail");
+const fs = require("fs").promises;
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const mailHelper = async (order, event) => {
-  console.log("inside mail helper")
-  const { email, name } = order
-  const { name: eventName, category, date } = event //TODO:also add contact name and number
+  console.log("inside mail helper");
+  const { email, name } = order;
+  const { name: eventName, category, date } = event; //TODO:also add contact name and number
 
-//converting date to readable format : month date
-const options = { month: 'long', day: 'numeric' }
-const dateString = date.toLocaleDateString('en-US', options)
+  //converting date to readable format : month date
+  const options = { month: "long", day: "numeric" };
+  const dateString = date.toLocaleDateString("en-US", options);
 
-
-  let html
+  let html;
   try {
     // Read HTML template file
-    html = await fs.readFile(__dirname + "/templates/workshop.html", "utf-8")
-    console.log("html file read successful")
+    html = await fs.readFile(__dirname + "/templates/workshop.html", "utf-8");
+    console.log("html file read successful");
   } catch (error) {
-    console.error("Error reading HTML template file:", error)
-    return "error"
+    console.error("Error reading HTML template file:", error);
+    return "error";
   }
 
-  if (category == "competitions") { //TODO: change it later to workshop
+  if (category == "competitions") {
+    //TODO: change it later to workshop
     // Replace placeholders in the HTML template with actual values
     const messageHtml = html
       .replace("{name}", name)
       .replace("{date}", dateString)
-      .replace("{eventName}", eventName)
+      .replace("{eventName}", eventName);
 
     // Send email with the modified HTML template
     const message = {
       to: email,
       from: {
-        name:"Invento",
-        email: process.env.EMAIL
+        name: "Invento",
+        email: process.env.EMAIL,
       },
       subject: `Confirmation of Registration: ${eventName} at INVENTO'23 - ${dateString} - Government Engineering College Palakkad`,
       html: messageHtml,
-    }
+    };
 
-    sgMail
-    .send(message)
-    .then(() => {
-      console.log('Email sent')
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+    try {
+      await sgMail.send(message);
+      console.log("email sent")
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   //if(category=="competitions")
-}
+};
 
-module.exports = mailHelper
+module.exports = mailHelper;
