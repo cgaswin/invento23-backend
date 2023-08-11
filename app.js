e0ba4b580b4feb6a97c58d6811dd68114cfa6149
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-08-11 22:01:37 +0530

    add some security features (#14)
    * code: remove duplicate import
    
    * feat: add basic rate limiting
    
    * feat: set secure http headers and also add a body limit
    
    * code: format

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const morgan = require("morgan")
 const cookieParser = require("cookie-parser")
 const fileUpload = require("express-fileupload")
 const cors = require("cors")
+const rateLimit = require("express-rate-limit")
+const helmet = require("helmet")
+
 const app = express()
 
-app.use(express.json())
+app.use(helmet())
+app.use(express.json({ limit: "10kb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(morgan("tiny"))
@@ -26,6 +30,15 @@ app.use(
 )
 
 app.set("view engine", "ejs")
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  legacyHeaders: false,
+  message: "Too many requests, please try again later.",
+})
+
+app.use("/api", limiter)
 
 app.get("/api/v1/newevent", (req, res) => {
   res.render("eventcreate")
--- validations/orderSchema.js
+++ validations/orderSchema.js
@@ -1,17 +1,19 @@
 const joi = require("joi")
 
 module.exports = joi.object({
-name:joi.string().required(),
-email:joi.string().required(),
-phone:joi.number().required(),
-referalCode:joi.string(),
-college:joi.string(),
-year:joi.number(),
-orderEvents:joi.array().items(joi.object({
-event:joi.string().required()
-})),
-paymentInfo:joi.object({
-id:joi.string().required()
-}),
-totalAmount:joi.number().required()
+  name: joi.string().required(),
+  email: joi.string().email().required(),
+  phone: joi.number().required(),
+  referalCode: joi.string(),
+  college: joi.string(),
+  year: joi.number(),
+  orderEvents: joi.array().items(
+    joi.object({
+      event: joi.string().required(),
+    })
+  ),
+  paymentInfo: joi.object({
+    id: joi.string().required(),
+  }),
+  totalAmount: joi.number().required(),
 })
