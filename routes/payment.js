5f737985e73f29161793307a973722963c80ba67
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-08-11 20:07:20 +0530

    code: remove duplicate import

--- routes/payment.js
+++ routes/payment.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const router = express.Router()
-const { captureRazorPayment } = require("../controllers/paymentController")
-const { verifyPayment } = require("../controllers/paymentController")
+const {
+  captureRazorPayment,
+  verifyPayment,
+} = require("../controllers/paymentController")
 
 router.route("/capturerazorpay").post(captureRazorPayment)
 router.route("/verify").post(verifyPayment)
