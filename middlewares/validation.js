6559e1f51d235a6978cdc47f3cc9ffd550f3bb27
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-08-11 21:33:31 +0530

    code: format

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,17 +1,17 @@
-
 const joi = require("joi")
 const CustomError = require("../errors/customError")
 
 const validation = (schema) => {
-return (req,res,next)=>{
-const {error} = schema.validate(req.body)
-if(error){
-const errorMessage = error.details.map((detail)=>detail.message).join(",")
-return next(new CustomError(errorMessage,400))
-}
-next()
-}
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body)
+    if (error) {
+      const errorMessage = error.details
+        .map((detail) => detail.message)
+        .join(",")
+      return next(new CustomError(errorMessage, 400))
+    }
+    next()
+  }
 }
 
 module.exports = validation
-
