352e727d4ed65e49285d60123b132726969d8967
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-08-22 16:31:39 +0530

    feat: accept payment screenshot

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,11 +1,12 @@
-const Order = require("../models/order");
-const Events = require("../models/event");
-const campusAmbassadors = require("../models/campusAmbassador");
-const Users = require("../models/user");
-const { createUser } = require("../controllers/user");
-const BigPromise = require("../middlewares/bigPromise");
-const CustomError = require("../errors/customError");
+const Order = require("../models/order")
+const Events = require("../models/event")
+const campusAmbassadors = require("../models/campusAmbassador")
+const Users = require("../models/user")
+const { createUser } = require("../controllers/user")
+const BigPromise = require("../middlewares/bigPromise")
+const CustomError = require("../errors/customError")
 const mailHelper = require("../utils/emailHelper")
+const cloudinary = require("cloudinary")
 
 exports.createOrder = BigPromise(async (req, res, next) => {
   const {
@@ -18,22 +19,24 @@ exports.createOrder = BigPromise(async (req, res, next) => {
     orderEvents,
     paymentInfo,
     totalAmount,
-    
-  } = req.body;
+  } = req.body
 
-  const orderEventsWithParticipants = orderEvents.map(event => {
+  const orderEventsWithParticipants = orderEvents.map((event) => {
     return {
       event: event.event,
       participants: event.participants, // Add participants array here
-    };
-  });
+    }
+  })
 
-  let file = null;
-  if (req.files && req.files.photo) {
-    file = req.files.photo;
-    result = await cloudinary.uploader.upload(file.tempFilePath, {
-      folder: "inventoPayment",
-    });
+  let file = null
+  let result
+  if (req.files && req.files[0]) {
+    file = req.files[0]
+
+    result = await cloudinary.v2.uploader.upload(file.path, {
+      // folder: "inventoPayment",
+      folder: "InventoVerifyPayment",
+    })
   }
 
   const order = await Order.create({
@@ -43,102 +46,41 @@ exports.createOrder = BigPromise(async (req, res, next) => {
     referalCode,
     college,
     year,
-    orderEvents:orderEventsWithParticipants,
+    orderEvents: orderEventsWithParticipants,
     paymentInfo,
     totalAmount,
     paymentProof: {
       id: result.public_id,
       secure_url: result.secure_url,
-    } 
-  });
+    },
+  })
 
-  const id = order._id;
+  if (referalCode) {
+    const ambassador = await campusAmbassadors.findOne({ referalCode })
 
-  if (id) {
-    updateOrder(id);
+    if (ambassador) {
+      order.referralVerified = true
+      await order.save({ validateBeforeSave: false })
+    }
+  }
+  for await (const event of order.orderEvents) {
+    const id = event.event
+    const singleEvent = await Events.findById(id)
+    await updateEventTicket(id)
+    await mailHelper(order, singleEvent)
   }
 
   res.status(200).json({
     success: true,
     order,
-  });
-});
+  })
+})
 
 exports.getAllOrders = BigPromise(async (req, res, next) => {
-  let orders = await Order.find();
-  console.log(orders);
+  let orders = await Order.find()
+
   res.status(200).json({
     success: true,
     orders,
-  });
-});
-
-async function updateOrder(id) {
-  const order = await Order.findById(id);
-  const email = order.email;
-
-  if (!order) {
-    return new CustomError("Please check order id", 401);
-  }
-
-  const user = await Users.findOne({ email });
-
-  if (!user) {
-    await createUser(order);
-  }
-  else{
-    for (const event of order.orderEvents) {
-      const id = event.event
-      const singleEvent = await Events.findById(id)
-      await updateUser(order.email, singleEvent.name, event.participants, order.referalCode);
-    }
-}
-
-for (const event of order.orderEvents) {
-  const id = event.event;
-  const singleEvent = await Events.findById(id);
-  await updateEventTicket(id);
-  await mailHelper(order,singleEvent);
-}
-
-if (order.referalCode) {
-  await updateCampusAmbassador(order.referalCode);
-}
-
-await order.save();
-
-}
-
-async function updateEventTicket(eventId) {
-  try {
-    const event = await Events.findById(eventId);
-    event.ticketsBooked = event.ticketsBooked + 1;
-    await event.save({ validateBeforeSave: false });
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-async function updateCampusAmbassador(referalCode) {
-  try {
-    const ambassador = await campusAmbassadors.findOne({ referalCode });
-    if (!ambassador) {
-      throw new CustomError("no ambassador found", 401);
-    }
-    ambassador.score = ambassador.score + 10;
-    await ambassador.save({ validateBeforeSave: false });
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-async function updateUser(email, eventName,participants, referalCode) {
-  const user = await Users.findOne({ email });
-  if (user) {
-    user.events.push({ eventName, participants });
-    if (referalCode && !user.referalCodes.includes(referalCode)) {
-      user.referalCodes.push(referalCode);
-    }
-    await user.save({ validateBeforeSave: false });
-  }
-}
+  })
+})
